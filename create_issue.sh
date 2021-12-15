6a929f63fb5232a56c86f98bbbd7026b9e20fdc2
athorat940 <92424910+athorat940@users.noreply.github.com>
2021-12-15 15:29:54 +0530

    Update create_issue.sh

--- create_issue.sh
+++ create_issue.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+rm -rf /opt/test/jenkins-pipeline-tutorial.git
 cd /opt/test
 git clone https://github.com/athorat940/jenkins-pipeline-tutorial.git
 cd /opt/test/jenkins-pipeline-tutorial
