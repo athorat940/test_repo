8e0e3885c1e70464ec90e22f9215aa1ce3dcb13c
athorat940 <92424910+athorat940@users.noreply.github.com>
2021-12-15 15:38:14 +0530

    Update create_issue.sh

--- create_issue.sh
+++ create_issue.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+gh auth login --with-token < secret.txt
 rm -rf /opt/test/jenkins-pipeline-tutorial.git
 cd /opt/test
 git clone https://github.com/athorat940/jenkins-pipeline-tutorial.git
