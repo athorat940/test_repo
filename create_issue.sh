0b4d72f8cf18dbff90fe58c81765a3ead6825983
athorat940 <92424910+athorat940@users.noreply.github.com>
2021-12-15 14:00:11 +0530

    Update create_issue.sh

--- create_issue.sh
+++ create_issue.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
-rm -rf /opt/test/jenkins-pipeline-tutorial
+gh auth login --with-token < secret.txt
 cd /opt/test
 git clone https://github.com/athorat940/jenkins-pipeline-tutorial.git
 cd /opt/test/jenkins-pipeline-tutorial
-git checkout feature
-cat >> xyz.txt <<- "EOF"
-"Hello everyone!!"
-EOF
-git add .
-git commit -m "Updated file"
-git push
-gh pr create --fill
-ranNum=$[RANDOM%60+1]
-echo "Please wait for $ranNum seconds to merge"
-sleep $ranNum's'
-#list=$(gh pr list | awk '{print $1}' | head -n1)
-git checkout master
-git merge feature -m "Merged into master"
-git push
+commit=$(git log |  awk '{print $2}' | head -n1)
+echo $commit
+gh issue create --title "Build Failure" --body "Root cause : $commit" --label "Incident"
